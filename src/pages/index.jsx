3a0b1e03c2e91668f3ed2b77becc498bb6ad2151
Morgan Segura <morgansegura@users.noreply.github.com>
2020-10-29 11:48:32 -0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,10 +24,10 @@ export default () => {
                     <div>
                         <h3 className="my-4 font-bold text-xl text-yellow-100">Some things I wish I could have finished:</h3>
                         <ol className="my-4 text-blue-100">
-                            <li>1. I did not have time to finish styling the blog properly. I definitely would have made the blog more dynamic, added pagination and a blog index page.</li>
+                            <li>1. I did not have time to finish styling the blog properly. I definitely would have made the blog more dynamic, added pagination and individual post pages.</li>
                             <li>2. I would have spent much more time with the contact form, styling, validation, toastr message, etc.</li>
                             <li>3. Given enough time I would have duplicated the entire site, and believe me, it was not easy for me to stop coding. I like to finish what I start, however in fairness and honesty I did not want to take more than the alotted time.</li>
-                            <li>4. Additionally given the chance to do it again, I would have focussed on the technical aspects first, then moved on to the pretty parts. I feel like I spent more time on the sponsors page and ran out of time for contant andd posts.</li>
+                            <li>4. Additionally given the chance to do it again, I would have focussed on the technical aspects first, then moved on to the pretty parts. I feel like I spent more time on the sponsors page and ran out of time to properly finish and test contact and posts.</li>
                             <li>5. I did not get a chance to validate the form, nor did I get a chance to ensure ith was working correctly.</li>
                         </ol>
                         <h3 className="my-4 font-bold text-xl text-yellow-100">Things I did for time's sake:</h3>
@@ -40,10 +40,10 @@ export default () => {
                         <h3 className="my-4 font-bold text-xl text-yellow-100">What I take from this project:</h3>
                         <ol className="my-4 text-blue-100">
                             <li>1. Although this task was not easy, I could have made it easier on myself had I known the full scope of the project. I like to plan things out first, this was a great excersize for thinking on my toes!</li>
-                            <li>2. I learned to move quickly, I setup most pages, nave and layouts first, then went in and started coding. </li>
-                            <li>3. I could have spent less time on the sponsors page, adn I did not have to do all of the sponsors, however in my opinion it was important for me to show you UI/UX and React experience.</li>
-                            <li>4. I could have setup graphql and pulled in the data but I ran out of time. Next time I won't fail ^^</li>
-                            <li>5. I usually commit more often, however in this case it was slowing me down a bit. So I pushed after very major edits, and toward the ned, almost not at all. Shame on me. ^^</li>
+                            <li>2. I learned to move quickly, I setup most pages, nav and layouts first, then went in and started coding. </li>
+                            <li>3. I could have spent less time on the sponsors page, and I did not have to do all of the sponsors, however in my opinion it was important for me to show you UI/UX and React experience.</li>
+                            <li>4. I could have setup GraphQL and pulled in the data but I ran out of time. Next time I won't fail ^^</li>
+                            <li>5. I usually commit more often, however in this case it was slowing me down a bit. So I pushed after very major edits, and toward the end, almost not at all. Shame on me. ^^</li>
                         </ol>
 
                         <h4 className="my-4 font-bold text-lg text-blue-100">Thank you a again for the opportunity! I hope to hear from you soon.</h4>
